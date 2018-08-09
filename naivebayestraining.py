8d4d4303cbba18ceaf8076dc5df6b14f87bd32dc
Larry-davis <larryd1914@gmail.com>
2018-08-09 11:11:00 -0500

    Finished part-of-speech classification
    Added more unittests to naivebayesunittests.py and

--- naivebayestraining.py
+++ naivebayestraining.py
@@ -6,6 +6,8 @@ Created by Larry Davis III
 """
 
 from readcsv import ReadCsv
+from stanfordcorenlp import StanfordCoreNLP
+from utilities import load_configuration_data
 import numpy as np
 
 
@@ -15,15 +17,45 @@ import numpy as np
 class TrainNaiveBayes:
 
     @staticmethod
-    def readcsv():
-        sentencelist = [ReadCsv.readcsv
-                        (
-                            r"C:\Users\elden\PycharmProjects\QuestionCreation-NaiveBayes\QuestionCreation-Naive_Bayes\Question Creation Documents\questiondata.csv")]
-        questionlist = [ReadCsv.readcsv
-                        (
-                            r"C:\Users\elden\PycharmProjects\QuestionCreation-NaiveBayes\QuestionCreation-Naive_Bayes\Question Creation Documents\questiondata.csv")]
-        print(questionlist, sentencelist)
-        return sentencelist, questionlist
+    def readcsv(file):
+        """
+        Reads given .csv file at file path
+
+        :param file: path of needed file
+        :return sentence_list: list of sentences in the first column of the .csv file
+        :return question_list: list of questions in the second column of the .csv file
+        """
+        global sentence_list
+        global question_list
+        sentence_list, question_list = ReadCsv.readcsv(file)
+        # print(sentence_list)
+        return sentence_list, question_list
+
+    @staticmethod
+    def partsofspeech(list_of_sentences, list_of_questions, configpath):
+        """
+        Parses sentences and questions into partsofspeech, etc using Stanford's corenlp library
+
+        :param list_of_sentences: list of sentences obtained by readcsv() function
+        :param list_of_questions: list of questions obtained by readcsv() function
+        :param configpath: config.json filepath, needed for corenlp library, described in its documentation
+        :return parsed_sentence_list: List of tuples that contain each word in a sentence and its part of speech
+        :return parsed_question_list: List of tuples that contain each word in a question and its part of speech
+        """
+        parsed_sentence_list = []
+        parsed_question_list = []
+
+        module_settings = load_configuration_data(configpath)
+
+        nlp = StanfordCoreNLP(module_settings["CORE_NLP"])
+
+        parsed_sentence_list = [nlp.pos_tag(sentence) for sentence in list_of_sentences]
+        parsed_question_list = [nlp.pos_tag(question) for question in list_of_questions]
+        print(parsed_question_list, parsed_sentence_list)
+        return parsed_sentence_list, parsed_question_list
 
 
+if __name__ == "__main__":
+    TrainNaiveBayes.readcsv(r"C:\Users\elden\PycharmProjects\QuestionCreationNaiveBayes\QuestionCreation-Naive_Bayes\Question Creation Documents\questiondata.csv")
+    TrainNaiveBayes.partsofspeech(sentence_list, question_list, r"C:\Users\elden\PycharmProjects\sentence-classifier\config.json")
 
--- naivebayesunittests.py
+++ naivebayesunittests.py
@@ -11,12 +11,19 @@ from naivebayestraining import TrainNaiveBayes
 class TestCsvReader(TestCase):
 
     def testSentenceList(self):
-        self.assertIsNotNone(TrainNaiveBayes.readcsv())
+        global sentencelist
+        global questionlist
+        sentencelist, questionlist = TrainNaiveBayes.readcsv(r"C:\Users\elden\PycharmProjects\QuestionCreationNaiveBayes\QuestionCreation-Naive_Bayes\Question Creation Documents\questiondata.csv")
+        self.assertIsNotNone(sentencelist)
+        self.assertNotEqual(sentencelist, questionlist)
+        self.assertEqual(len(sentencelist), len(questionlist))
 
     def testParser(self):
-        pass
+        parsedsentences, parsedquestions = TrainNaiveBayes.partsofspeech(sentencelist, questionlist, r"C:\Users\elden\PycharmProjects\sentence-classifier\config.json")
+        self.assertIsInstance(parsedsentences[0], tuple, "Did not return tuples with parts of speech")
 
-    def test
+    def test(self):
+        pass
 
 
 if __name__ == "__main__":
